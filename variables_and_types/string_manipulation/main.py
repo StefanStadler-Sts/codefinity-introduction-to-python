61400c842e823e817e4d4f527c7c2e155a57eeb0
StefanStadler-Sts <>
2026-06-04 07:46:07 +0000

    String manipulation

---
+++ variables_and_types/string_manipulation/main.py
@@ -0,0 +1,17 @@
+grocery_items = "milk cheese bread apples oranges chicken"
+milk = grocery_items[0:4]
+cheese = grocery_items[5:11]
+bread = grocery_items[12:17]
+apples = grocery_items[18:24]
+oranges = grocery_items[25:32]
+chicken = grocery_items[33:]
+
+print(milk)
+print(cheese)
+print(bread)
+print(apples)
+print(oranges)
+print(chicken)
+
+combined = milk + " " + cheese + " " + bread + " " + apples + " " + oranges + " " + chicken
+print (combined)
